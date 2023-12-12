d3be55d917736db39b764a331ce44f7ea00f228c
조영민 <sadudals12@gmail.com>
2023-12-12 15:48:28 +0900

    add hello method

--- src/Spring/tobys/testcode/project1.java
+++ src/Spring/tobys/testcode/project1.java
@@ -1,7 +1,12 @@
 package Spring.tobys.testcode;
 
 public class project1 {
-
+	
+	
+	private static void hello() {
+		System.out.println("Hello method");
+	}
+	
 	public static void main(String[] args) {
 
 		System.out.println("iss533");
