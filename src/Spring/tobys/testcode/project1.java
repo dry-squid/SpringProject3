95bd23bc5e6ba20f3d10e304bdd72d70ffd2226a
조영민 <sadudals12@gmail.com>
2023-12-12 15:14:07 +0900

    1st code add

--- src/Spring/tobys/testcode/project1.java
+++ src/Spring/tobys/testcode/project1.java
@@ -3,8 +3,9 @@ package Spring.tobys.testcode;
 public class project1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("iss533");
+		
 	}
 
 }
