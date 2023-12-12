e8dd7a2fa2acf37bb94ef1efd86506185105d9f3
dry-squid <148878471+dry-squid@users.noreply.github.com>
2023-12-12 17:08:04 +0900

    Update project1.java

--- src/Spring/tobys/testcode/project1.java
+++ src/Spring/tobys/testcode/project1.java
@@ -14,6 +14,7 @@ public class project1 {
 	public static void main(String[] args) {
 
 		System.out.println("Hello remote modification");
+		System.out.println("Hello remote modification");
 	}
 
 }
